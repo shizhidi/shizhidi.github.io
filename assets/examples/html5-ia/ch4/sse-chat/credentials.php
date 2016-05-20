767bda266de61f59bf937a55ec69308ed69a17d2
shizhidi <shizhidi@gmail.com>
2016-05-20 22:38:45 +0800

    Site updated: 2016-05-20 22:38:43

--- assets/examples/html5-ia/ch4/sse-chat/credentials.php
+++
@@ -1,5 +0,0 @@
-<?php
-$user = "root";
-$pass = "";
-$db = "mysql:host=localhost;dbname=ssechat"; //Something like "mysql:host=localhost;dbname=ssechat"
-?>
--- assets/examples/html5-ia/ch8/readme.txt
+++
@@ -1,1 +0,0 @@
-NOTE: The videos used in this example were filmed on top of a hill with a phone, consequently there is a lot of wind noise.
