daf8a8ea9b91b39105235bf4ca32da5ce4c11a7f
shizhidi <shizhidi@gmail.com>
2016-06-23 22:27:50 +0800

    Site updated: 2016-06-23 22:26:49

--- assets/examples/html5-ia/ch6/game.js
+++ assets/examples/html5-ia/ch6/game.js
@@ -21,7 +21,7 @@ Publisher: Manning
         };
     })();
 
-    // Residing place for our Canvas' context
+    // 一个用来容纳2D绘图环境的空变量。
     var ctx = null;
 
     var Game = {
@@ -32,19 +32,20 @@ Publisher: Manning
                 // Setup variables
                 ctx = this.canvas.getContext('2d');
 
-                // Cache width and height of the Canvas to save processing power
+                // 从canvas元素中获取宽高值。
                 this.width = this.canvas.width;
                 this.height = this.canvas.height;
 
                 // Run the game
                 Screen.welcome();
+                //添加新的事件侦听器
                 this.canvas.addEventListener('click', this.runGame, false);
                 Ctrl.init();
             }
         },
 
         // Setup initial objects
-        init: function() {
+        init: function() { //包含了所有的对象实例。
             Background.init();
             Hud.init();
             Bricks.init();
@@ -52,13 +53,15 @@ Publisher: Manning
             Paddle.init();
         },
 
-        // Run from the global space, so you must use Game instead of this to prevent a crash
+        //animate函数经常在被调用时引用自身。
         animate: function() {
+            //由于animate()是个在Game对象之外触发的自引用函数,所以必须引用Game对象,而非this
             Game.play = requestAnimFrame(Game.animate);
             Game.draw();
         },
-
+        //draw()用于处理所有更新并绘制对象的逻辑。
         draw: function() {
+            //将Canvas绘图板清空,每次更新它时,之前绘制的图形就会被清除。
             ctx.clearRect(0, 0, this.width, this.height);
 
             // Draw objects
@@ -71,6 +74,7 @@ Publisher: Manning
 
         // Must reference as Game isntead of this due to when the listener is fired (outside of the object)
         runGame: function() {
+            //事件触发后就去除事件侦听器
             Game.canvas.removeEventListener('click', Game.runGame, false);
             Game.init();
 
@@ -85,14 +89,14 @@ Publisher: Manning
         },
 
         // Leveling
-        levelUp: function() {
+        levelUp: function() { //每当关卡增加时,就会触发升级逻辑。
             Hud.lv += 1;
             Bricks.init();
             Ball.init();
             Paddle.init();
         },
 
-        levelLimit: function(lv) {
+        levelLimit: function(lv) { //将砖块限定为最高只能达到5行。
             return lv > 5 ? 5 : lv;
         }
     };
@@ -116,10 +120,10 @@ Publisher: Manning
             this.create();
         },
 
-        create: function() {
+        create: function() { //只输出设定的参数,以便于必要时调整界面文本。
             // Background
             ctx.fillStyle = 'black';
-            ctx.fillRect(0, 0, Game.width, Game.height);
+            ctx.fillRect(0, 0, Game.width, Game.height); //背景
 
             // Main text
             ctx.fillStyle = this.textColor;
@@ -139,7 +143,7 @@ Publisher: Manning
     ***************************/
     var Background = {
         init: function() {
-            // Makes sure nothing is drawn until the image is fully loaded
+            // Canvas需要一个Image对象来绘制背景。将源文件中的背景图片的文件名赋给Image.src
             this.ready = false;
 
             // Createa a background image
@@ -155,14 +159,14 @@ Publisher: Manning
             }
         }
     };
-
+    //创建小球
     var Ball = {
-        r: 10,
+        r: 10, //用于确定小球大小的半径变量r
 
-        init: function() {
+        init: function() { //this.sx是x轴上的速度增量,this.sy是y轴上的速度增量。
             this.x = 120;
             this.y = 120;
-            this.sx = 1 + (0.4 * Hud.lv);
+            this.sx = 1 + (0.4 * Hud.lv); //小球的速度要和当前所处关卡匹配起来。
             this.sy = -1.5 - (0.4 * Hud.lv);
         },
 
@@ -189,11 +193,12 @@ Publisher: Manning
         // Edge dectection 小球碰撞检测
         edges: function() {
             // Top / bottom
-            if (this.y < 1) {
+            if (this.y < 1) { //游戏容器的上边界
                 this.y = 1; // Prevents the ball from getting stuck at fast speeds
                 this.sy = -this.sy;
-            } else if (this.y > Game.height) { // Bottom
+            } else if (this.y > Game.height) { // 容器的下边界
                 // Stop the ball and hide it
+                //用一些方法和对象来隐藏小球并触发游戏结束状态。
                 this.sy = this.sx = 0;
                 this.y = this.x = 1000;
 
@@ -204,21 +209,22 @@ Publisher: Manning
             }
 
             // Sides
-            if (this.x < 1) {
+            if (this.x < 1) { //容器的左边界
                 this.x = 1; // Prevents the ball from getting stuck at fast speeds
                 this.sx = -this.sx;
-            } else if (this.x > Game.width) { // Right
+            } else if (this.x > Game.width) { // 容器的右边界
                 this.x = Game.width - 1; // Prevents the ball from getting stuck at fast speeds
                 this.sx = -this.sx;
             }
         },
 
-        // Paddle to ball collision detection 小球接触球拍
+        //  小球接触球拍
         collide: function() {
             if (this.x >= Paddle.x &&
                 this.x <= (Paddle.x + Paddle.w) &&
                 this.y >= Paddle.y &&
                 this.y <= (Paddle.y + Paddle.h)) {
+                //根据小球碰撞到球拍上的具体位置,修改小球在偏向弹回时的x坐标。
                 this.sx = 7 * ((this.x - (Paddle.x + Paddle.w / 2)) / Paddle.w);
                 this.sy = -this.sy;
             }
@@ -233,7 +239,7 @@ Publisher: Manning
         init: function() {
             this.x = 100;
             this.y = 210;
-            this.speed = 4; //用来控制球拍的速度,以后会进行配置
+            this.speed = 4; //用来控制球拍的速度
         },
 
         draw: function() {
@@ -260,7 +266,7 @@ Publisher: Manning
             ctx.arcTo(this.x, this.y + this.h, this.x, this.y + this.h - this.r, this.r);
             ctx.lineTo(this.x, this.y + this.r);
             ctx.arcTo(this.x, this.y, this.x + this.r, this.y, this.r);
-            ctx.closePath();
+            ctx.closePath(); //关闭路径。这能防止出现一些图像撕裂和对象消失等错误。
 
             ctx.fillStyle = this.gradient();
             ctx.fill();
@@ -294,11 +300,12 @@ Publisher: Manning
         w: 80,
         h: 15,
         init: function() {
-            this.row = 2 + Game.levelLimit(Hud.lv);
+            this.row = 2 + Game.levelLimit(Hud.lv);//保证砖块不会因为行数过多而溢出游戏界面。
             this.total = 0;
 
             // Create an updatable brick array = number of bricks
             this.count = [this.row];
+            // 砖块数组由砖块的行列号数据所构成。
             for (var i = this.row; i--;) {
                 this.count[i] = [this.col];
             }
@@ -319,7 +326,7 @@ Publisher: Manning
                             continue;
                         }
 
-                        ctx.fillStyle = this.gradient(i);
+                        ctx.fillStyle = this.gradient(i);//将自动根据砖块所属的行号,使同一列砖块的颜色形成漂亮的渐变。
                         ctx.fillRect(this.x(j), this.y(i), this.w, this.h);
                     }
                 }
@@ -339,15 +346,15 @@ Publisher: Manning
         },
         //如果发生重叠,将其设定为false,并将小球的y坐标取负
         collide: function(i, j) {
-            Hud.score += 1;
-            this.total += 1;
+            Hud.score += 1; //每当砖块被摧毁时,就递增得分计数器。
+            this.total += 1; //递增砖块总数,以便让游戏知道何时结束__所有砖块都摧毁了。
             this.count[i][j] = false;
             Ball.sy = -Ball.sy;
         },
-
+        //对砖块进行着色
         gradient: function(row) {
             switch(row) {
-                case 0: // purple
+                case 0: // purple 如果存在缓存渐变,就使用它;如果不存在,就创建一个新的渐变。
                     return this.gradientPurple ?
                         this.gradientPurple :
                         this.gradientPurple = this.makeGradient(row, '#bd06f9', '#9604c7');
@@ -368,7 +375,9 @@ Publisher: Manning
 
         makeGradient: function(row, color1, color2) {
             var y = this.y(row);
+            //在特定位置创建一个新的线性渐变。
             var grad = ctx.createLinearGradient(0, y, 0, y + this.h);
+            //渐变起始颜色为color1,终止颜色为color2
             grad.addColorStop(0, color1);
             grad.addColorStop(1, color2);
 
@@ -386,9 +395,9 @@ Publisher: Manning
             ctx.font = '12px helvetica, arial';
             ctx.fillStyle = 'white';
             ctx.textAlign = 'left';
-            ctx.fillText('Score: ' + this.score, 5, Game.height - 5);
+            ctx.fillText('Score: ' + this.score, 5, Game.height - 5); //创建得分文本。
             ctx.textAlign = 'right';
-            ctx.fillText('Lv: ' + this.lv, Game.width - 5, Game.height - 5);
+            ctx.fillText('Lv: ' + this.lv, Game.width - 5, Game.height - 5); //创建关卡文本
         }
     };
 
@@ -436,21 +445,23 @@ Publisher: Manning
             }
         },
 
-        // Prevents default scrolling on touch devices
+        // 屏蔽触摸移动的默认功能
         stopTouchScroll: function(event) {
             event.preventDefault();
         },
 
         movePaddle: function(event) {
             var mouseX = event.pageX;    //鼠标指针的x坐标
+
+            //从浏览器窗口的左边界到Canvas元素的距离(以像素计)
             var canvasX = Game.canvas.offsetLeft;
             var paddleMid = Paddle.w / 2;
 
             if (mouseX > canvasX &&
                 mouseX < canvasX + Game.width) {
                 var newX = mouseX - canvasX;
-                newX -= paddleMid;
-                Paddle.x = newX;
+                newX -= paddleMid; //偏移球拍的新位置,以便让球拍与鼠标指针对齐。
+                Paddle.x = newX; //劫持已有的Paddle对象,更换新的x坐标值。
             }
         }
     };
